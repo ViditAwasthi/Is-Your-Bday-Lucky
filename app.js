dad5ebc013381ff9a0f79b80ea2d7bced2c94c86
Vidit Awasthi <viditawasthi2@gmail.com>
2021-08-05 00:39:56 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const birthDate = document.querySelector("#birth-date");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("#check-button");
+const resultDiv = document.querySelector("#result-div");
+
