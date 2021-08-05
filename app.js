8b812fd8b4c47dd3a96895e8c97e00bb739c9202
Vidit Awasthi <viditawasthi2@gmail.com>
2021-08-05 20:42:19 +0530

    fix: formatting

--- app.js
+++ app.js
@@ -4,36 +4,35 @@ const checkButton = document.querySelector("#check-button");
 const resultDiv = document.querySelector("#result-div");
 
 
-function checkBirthDateLucky(){
-    if(birthDate.value&&luckyNumber.value){
-    const dateOfBirth = birthDate.value;
-    const sum = calculateSum(dateOfBirth);
-    compareBirthdate(sum, luckyNumber.value);
-    }else{
-        resultDiv.innerText="Please Enter the Values to Check!😊"
+function checkBirthDateLucky() {
+    if (birthDate.value && luckyNumber.value) {
+        const dateOfBirth = birthDate.value;
+        const sum = calculateSum(dateOfBirth);
+        compareBirthdate(sum, luckyNumber.value);
+    } else {
+        resultDiv.innerText = "Please Enter the Values to Check!😊"
     }
 
 }
 
-function calculateSum(dateOfBirth){
-   dateOfBirth = dateOfBirth.replaceAll("-","");
-   let sum =0;
-   for(let i =0; i<dateOfBirth.length;i++){
-     sum = sum + Number(dateOfBirth.charAt(i))
-   }
-   return sum;
+function calculateSum(dateOfBirth) {
+    dateOfBirth = dateOfBirth.replaceAll("-", "");
+    let sum = 0;
+    for (let i = 0; i < dateOfBirth.length; i++) {
+        sum = sum + Number(dateOfBirth.charAt(i))
+    }
+    return sum;
 }
 
-function compareBirthdate(sum, luckyNumber){
-    if(sum%luckyNumber === 0){
-        resultDiv.innerText="Congrats! Your Birthdate is Lucky🥳"
-     
-    }else{
-        resultDiv.innerText="Oh! Your Birthdate is Not Lucky☹️"
-        
+function compareBirthdate(sum, luckyNumber) {
+    if (sum % luckyNumber === 0) {
+        resultDiv.innerText = "Congrats! Your Birthdate is Lucky🥳"
+
+    } else {
+        resultDiv.innerText = "Oh! Your Birthdate is Not Lucky☹️"
+
     }
 
 }
 
 checkButton.addEventListener("click", checkBirthDateLucky)
-
